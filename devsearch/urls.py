a7b5234d26e07e0924e484045ade57b503412b77
Antony Hrobust <antoonhrobust@gmail.com>
2023-02-09 01:05:20 +0100

    Implemented templates system

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', admin.site.urls),
-    path('profiles/', admin.site.urls),
+    path('', include('projects.urls')),
 ]

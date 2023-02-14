0a2b1902e7a11eeab01754b8c2764af32c0fd1c2
Antony Hrobust <antoonhrobust@gmail.com>
2023-02-15 00:25:48 +0100

    Added and customized theme

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project, Review, Tag
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs): # 
+        super(ProjectForm, self).__init__(*args, **kwargs) 
+        self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Project Title'})
+        self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Project Description'})
+        self.fields['demo_link'].widget.attrs.update({'class': 'input', 'placeholder': 'Demo Link'})
+        self.fields['source_link'].widget.attrs.update({'class': 'input', 'placeholder': 'Source Link'})
+

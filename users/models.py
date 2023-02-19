1e8455288fa299d79a20db561c13df19b951e10c
Antony Hrobust <antoonhrobust@gmail.com>
2023-02-19 13:10:04 +0100

    Add and Render Profiles

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=200, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
@@ -15,9 +16,21 @@ class Profile(models.Model):
     social_linkedin = models.CharField(max_length=200, null=True, blank=True)
     social_youtube = models.CharField(max_length=200, null=True, blank=True)
     social_website = models.CharField(max_length=200, null=True, blank=True)
+    social_stackoverflow = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.name
-
--- users/views.py
+++ users/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .models import Profile
+
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {
+        'profiles': profiles
+    }
+
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="") # Exclude skills with no description
+    otherSkills = profile.skill_set.filter(description__exact="") # Include skills with no description
+
+    context = {
+        'profile': profile,
+        'topSkills': topSkills,
+        'otherSkills': otherSkills
+    }
+
+    return render(request, 'users/user-profile.html', context)

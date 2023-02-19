0368d6d7b388fd8bfb6d6f8c921b3859c71b699b
Antony Hrobust <antoonhrobust@gmail.com>
2023-02-19 15:09:45 +0100

    Added signals

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -34,3 +35,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+

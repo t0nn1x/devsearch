1c0c16ebc669d6a7ac517eb1de8442d4e352d69a
t0nn1x <antoonhrobust@gmail.com>
2023-03-17 16:00:05 +0100

    Created User Profile

--- users/views.py
+++ users/views.py
@@ -80,3 +80,18 @@ def userProfile(request, pk):
     }
 
     return render(request, 'users/user-profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.projects.all()
+
+    context = {
+        'profile': profile,
+        'skills': skills,
+        'projects': projects
+        }
+    
+    return render(request, 'users/account.html', context)

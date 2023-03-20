51ce434f6088ae2ed3a36bab6defea13d1f1b758
t0nn1x <antoonhrobust@gmail.com>
2023-03-20 01:43:55 +0100

    Added CRUD operations for User Project

--- projects/views.py
+++ projects/views.py
@@ -18,12 +18,15 @@ def project(request, pk):
 @login_required(login_url='login')
 def createProject(request):
     """This function is used to create a project."""
+    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect('projects')
 
     context = {'form': form}
@@ -32,7 +35,8 @@ def createProject(request):
 @login_required(login_url='login')
 def updateProject(request, pk):
     """This function is used to update a project."""
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.projects.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
@@ -47,7 +51,8 @@ def updateProject(request, pk):
 @login_required(login_url='login')
 def deleteProject(request, pk):
     """This function is used to delete a project."""
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.projects.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 def loginUser(request):
     page = 'login'
@@ -48,7 +48,7 @@ def registerUser(request):
             messages.success(request, "Account was created for " + username)
 
             login(request, user)
-            return redirect('login')
+            return redirect('edit-account')
         else:
             messages.error(request, "An error has occurred during registration")
         
@@ -95,3 +95,20 @@ def userAccount(request):
         }
     
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
+    context = {'form': form}
+
+    return render(request, 'users/profile_form.html', context)

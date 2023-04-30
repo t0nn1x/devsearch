6bce82d6c154020144e28ebebb8421009f702641
t0nn1x <antoonhrobust@gmail.com>
2023-05-01 00:06:54 +0200

    Setted up POST requests

--- api/urls.py
+++ api/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('projects/<str:pk>/', views.getProject),
     path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+    path('projects/<str:pk>/vote/', views.projectVote, name='projects-vote')
+]   

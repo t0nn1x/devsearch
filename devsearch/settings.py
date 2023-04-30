05d2f5418812ac8e187571dcac44742504be0351
t0nn1x <antoonhrobust@gmail.com>
2023-04-30 22:49:54 +0200

    Nested Serializers and Serializer Method Fields

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -125,7 +125,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "devsearch.post@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "" # ltunxngtlzihewlv
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- projects/models.py
+++ projects/models.py
@@ -69,5 +69,4 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
     
